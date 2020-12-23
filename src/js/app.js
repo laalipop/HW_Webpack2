1ea3f213de1817dd1d0de763c7dae027839f63d4
laalipop <preicpodhya@mail.ru>
2020-12-23 15:58:08 +0300

    add files

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+const game = new Game();
+game.start();
+
+import Game, { loadGame, saveGame } from './game.js';
+
+import sum from './basic';
+
 console.log('worked');
 
 console.log(sum([1, 2]));
---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
+import Character from './domain.js';
+
+export default Game;
